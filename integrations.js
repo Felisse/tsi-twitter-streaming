784b016b3720816c1411dae7dc189d68d0a718a8
Philippe PETER <github@philpeter.com>
2016-12-16 18:34:15 +0100

    First API TSI
    Get, Put

--- integrations.js
+++ integrations.js
@@ -1,7 +1,7 @@
 // integrations.js
 
-var _server = require('./conf/intelligence.js')
+var _server = require('./conf/intelligence.js');
 
 // TrueSight Intelligence
-require('./integrations/intelligence.js')
-console.info('Integration: TrueSight Intelligence found. Using API: ' + _server.tsi_endpoint )
+require('./integrations/intelligence.js');
+console.info('Integration: TrueSight Intelligence found. Using API: ' + _server.tsi_endpoint );
--- integrations/intelligence.js
+++ integrations/intelligence.js
@@ -1,27 +1,58 @@
 // integrations/intelligence.js
 
+// Configuration
+var _server = require('../conf/intelligence.js')
+
 var _tsiRequest = require('request')
+require('request-debug')(_tsiRequest);
 
 var exports = module.exports = {}
 
 exports.tsiAPICall = function ( apiContext, postData, callback) {
 
-  var auth = "Basic " + new Buffer(_server.tsi_username + ':' + _server.tsi_token).toString("base64")
-
+  // URL definition
   var url  = _server.tsi_endpoint + apiContext
 
-  var callOptions = {
-    url: url,
-    body: JSON.stringify(postData),
-    auth: {
-      user: _server.tsi_username,
-      pass: _server.tsi_token,
-      sendImmediately: true
-    },
-    headers: {
-      'Content-Type': 'application/json'
+  // Postdata management & Option definition
+  if (postData) {
+    var callOptions = {
+      url: url,
+      body: JSON.stringify(postData),
+      auth: {
+        user: _server.tsi_username,
+        pass: _server.tsi_token,
+        sendImmediately: true
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  }
+  else {
+    var callOptions = {
+      url: url,
+      auth: {
+        user: _server.tsi_username,
+        pass: _server.tsi_token,
+        sendImmediately: true
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
   }
 
+  // Debug
+  console.log(callOptions)
+
+  // If nothing posted, then we do a GET
+  if (!postData) {
+    console.log(postData)
+    _tsiRequest.get(callOptions, callback)
+    return;
+  }
+
+  // If not...
+  _tsiRequest.post(callOptions, callback);
 
 }
